985edb5a4d8da1dd33299e54c3e8ee8832a5bf89
Daria Jung <djj2115cu@gmail.com>
2014-08-07 13:01:11 -0400

    kth smallest

---
+++ data_structures_algos/kth_smallest.py
@@ -0,0 +1,16 @@
+# Given a list of numbers in random order write a 
+# linear time algorithm to find the kth smallest 
+# number in the list. Explain why your algorithm is linear.
+
+def k_smallest(_list, k):
+    d = dict()
+    for i in _list:
+        d[i] = "whatever"
+
+    for i, v in enumerate(d):
+        if i == (k - 1):
+            return v
+
+if __name__ == "__main__":
+    test = [7, 4, 8, 11, 2, 5]
+    print k_smallest(test, 1)
