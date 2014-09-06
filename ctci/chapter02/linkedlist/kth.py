64cb5f9def6656e01b4acbf15be4c5cb850f479a
Daria Jung <djj2115cu@gmail.com>
2014-09-06 14:53:59 -0400

    find kth element from end in linked list

---
+++ ctci/chapter02/linkedlist/kth.py
@@ -0,0 +1,29 @@
+# implement an algorithm to find the kth to last element in a linked list
+
+import random
+import linkedlist
+
+def kth_to_last(_list, k):
+    _list.reverse()
+
+    current = _list.head
+    counter = 1
+
+    while counter < k:
+        current = current.next
+        counter += 1
+
+    return current.value 
+
+
+
+if __name__ == "__main__":
+    linked = linkedlist.LinkedList()
+
+    for i in range(20):
+        linked.push(random.randint(0, 10))
+
+    linked.traverse()
+    print ("\n------------")
+
+    print kth_to_last(linked, 3)
